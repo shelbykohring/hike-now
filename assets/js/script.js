f0db271c36a1bd7d5a6ba4a3a6cb4dd8792d6943
jelly octane <jamie.de.lange@mac.com>
2020-11-16 13:48:50 -0600

    initial push 2.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,27 @@
+$(".button2").click(function() {
+    $(".container").show();
+    $(".back-button").show();
+});
+$(".button2").click(function() {
+    $(".card2").hide();
+});
+$(".back-button").click(function() {
+    $(".card2").show();
+    $(".back-button").hide();
+});
+$(".back-button").click(function() {
+    $(".container").hide();
+});
+
 //Movement animation piece
 const card = document.querySelector('.card');
 const container = document.querySelector('.container');
 const title = document.querySelector('.title');
-const nzl = document.querySelector('.nzl');
+const map = document.querySelector('.map');
 const description = document.querySelector(".description");
 const list = document.querySelector(".list");
 const moreinfo = document.querySelector(".more-info");
+const weather = document.querySelector(".weather");
 
 //Animation event
 container.addEventListener('mousemove', (e) => {
@@ -17,11 +33,11 @@ container.addEventListener('mousemove', (e) => {
 container.addEventListener("mouseenter", (e) => {
     card.style.transition = "none";
     //Pop out
-    title.style.transform = "translateZ(100px)";
-    nzl.style.transform = "translateZ(80px)";
+    title.style.transform = "translateZ(80px)";
+    map.style.transform = "translateZ(50px)";
     description.style.transform = "translateZ(50px)";
-    list.style.transform = "translateZ(70px)";
-    moreinfo.style.transform = "translateZ(90px)";
+    weather.style.transform = "translateZ(60px)";
+    moreinfo.style.transform = "translateZ(80px)";
 });
 //Animate out
 container.addEventListener("mouseleave", (e) => {
@@ -29,8 +45,150 @@ container.addEventListener("mouseleave", (e) => {
     card.style.transform = `rotateY(0deg) rotateX(0deg)`;
     //Pop back
     title.style.transform = "translateZ(0px)";
-    nzl.style.transform = "translateZ(0px)";
+    map.style.transform = "translateZ(0px)";
     description.style.transform = "translateZ(0px)";
-    list.style.transform = "translateZ(0px)";
+    weather.style.transform = "translateZ(0px)";
     moreinfo.style.transform = "translateZ(0px)";
 });
+
+// -----------------------------------------------------------------------------------------
+var currentTime = (moment().format('MM/DD/YYYY'));
+var apiKey = "78abac7397dbff0934df4ef82fc5fd58";
+var query = document.getElementById("search-term");
+var maps = document.getElementById("map");
+
+var images = {
+    Alabama: "./assets/images/alabama.png",
+    Alaska: "./assets/images/alaska.png",
+    Arizona: "./assets/images/arizona.png",
+    Arkansas: "./assets/images/arkansas.png",
+    California: "./assets/images/california.png",
+    Colorado: "./assets/images/colorado.png",
+    Connecticut: "./assets/images/connecticut.png",
+    Delaware: "./assets/images/delaware.png",
+    Florida: "./assets/images/florida.png",
+    Georgia: "./assets/images/georgia.png",
+    Hawaii: "./assets/images/hawaii.png",
+    Idaho: "./assets/images/idaho.png",
+    Illinois: "./assets/images/illinois.png",
+    Indiana: "./assets/images/indiana.png",
+    Iowa: "./assets/images/iowa.png",
+    Kansas: "./assets/images/kansas.png",
+    Kentucky: "./assets/images/kentucky.png",
+    Louisiana: "./assets/images/louisiana.png",
+    Maine: "./assets/images/maine.png",
+    Maryland: "./assets/images/maryland.png",
+    Massachusetts: "./assets/images/massachusetts.png",
+    Michigan: "./assets/images/michigan.png",
+    Minnesota: "./assets/images/minnesota.png",
+    Mississippi: "./assets/images/mississippi.png",
+    Missouri: "./assets/images/missouri.png",
+    Montana: "./assets/images/montana.png",
+    Nebraska: "./assets/images/nebraska.png",
+    Nevada: "./assets/images/nevada.png",
+    "New Hampshire": "./assets/images/new-hampshire.png",
+    "New Jersey": "./assets/images/new-jersey.png",
+    "New Mexico": "./assets/images/new-mexico.png",
+    "New York": "./assets/images/new-york.png",
+    "North Carolina": "./assets/images/north-carolina.png",
+    "North Dakota": "./assets/images/north-dakota.png",
+    Ohio: "./assets/images/ohio.png",
+    Oklahoma: "./assets/images/oklahoma.png",
+    Oregon: "./assets/images/oregon.png",
+    Pennsylvania: "./assets/images/pennsylvania.png",
+    "Rhode Island": "./assets/images/rhode-island.png",
+    "South Carolina": "./assets/images/south-carolina.png",
+    "South Dakota": "./assets/images/south-dakota.png",
+    Tennessee: "./assets/images/tennessee.png",
+    Texas: "./assets/images/texas.png",
+    Utah: "./assets/images/utah.png",
+    Vermont: "./assets/images/vermont.png",
+    Virginia: "./assets/images/virginia.png",
+    Washington: "./assets/images/washington.png",
+    "West Virginia": "./assets/images/west-virginia.png",
+    Wisconsin: "./assets/images/wisconsin.png",
+    Wyoming: "./assets/images/wyoming.png"
+};
+
+console.log(currentTime);
+
+$("#search-term").keypress(function(e){
+    if(e.which == 13) {
+        $("#button2").click();
+    }
+});
+
+// UV index
+function uvIndex(lng, lat) {
+    $.ajax({
+        url: `https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lng}`,
+        method: "GET"
+    })
+        .then(function (response1) {
+
+            $(".uvIndex").html("UV Index: " + `<span class=" badge badge-danger">${(response1.value)}</span>`);
+        });
+}
+
+let geocoder;
+
+function successFunction(position) {
+    let lat = position.coords.latitude;
+    let lng = position.coords.longitude;
+    console.log(lat, lng)
+
+    $.ajax({
+        url: `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=imperial&appid=${apiKey}`,
+        method: "GET"
+    })
+        .then(function (response) {
+            updateLocation(response);
+        });
+    uvIndex(lng, lat);
+};
+function updateLocation(response) {
+    $(".city").html(`<h2>${response.name} (${currentTime}) <img src="https://openweathermap.org/img/w/${response.weather[0].icon}.png"></h2>`);
+    $(".humidity").text("Humidity: " + Math.round(response.main.humidity) + "%");
+    $(".temperature").text("Temperature: " + Math.round(response.main.temp) + "°F");
+    $(".title").html(response.name);
+    console.log(response.name);
+    maps.src = images[response.name];
+}
+function errorFunction() {
+    alert("Geocoder failed");
+}
+
+function citySearch() {
+    $(".button2").click(function (event) {
+        //this event prevents default refreshing of the page upon button click
+        event.preventDefault("click")
+        let city = $("#search-term").val().trim();
+        if (city != '') {
+            $.ajax({
+                url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`,
+                method: "GET"
+            }).then(function (response) {
+                updateLocation(response);
+                uvIndex(response.coord.lon, response.coord.lat)
+            });
+        }
+    })
+};
+citySearch();
+
+const cityInfo = async (city) => {
+
+    const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+    const response = await fetch(queryURL);
+    const result = await response.json();
+    return result;
+
+};
+
+const buttonClick = (city) => {
+
+    cityInfo(city).then(response => {
+        updateLocation(response);
+        uvIndex(response.coord.lon, response.coord.lat);
+    });
+};
